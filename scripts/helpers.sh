c7b09046d7d1712b8d6d7a8ece47123f4b9ae090
Javier Lopez <m@javier.io>
2015-06-24 23:48:11 -0500

    refactoring

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -37,7 +37,7 @@ _get_digits_from_string_helper() {
 _get_tmux_option_helper() {
     [ -z "${1}" ] && return 1
 
-    if [ "${TMUX_VERSION}" -ge "19" ]; then
+    if [ "${TMUX_VERSION-16}" -ge "19" ]; then
         _gtohelper__value="$(tmux show-option -gqv "${1}")"
     else #tmux => 1.6 altough could work on even lower tmux versions
         _gtohelper__value="$(tmux show-option -g|awk "/^${1}/ {gsub(/\'/,\"\");gsub(/\"/,\"\"); print \$2; exit;}")"
@@ -60,6 +60,25 @@ _get_tmux_environment_helper() {
     else
         printf "%s\\n" "${_gtehelper__value}"
     fi
+}
+
+_get_tmux_option_global_helper() {
+    [ -z "${1}" ] && return 1
+    _gtoghelper__option="$(_get_tmux_environment_helper "${1}")"
+    [ -z "${_gtoghelper__option}" ] && \
+        _get_tmux_option_helper "${1}" "${2}" || \
+        printf "%s" "${_gtoghelper__option}"
+}
+
+_supported_tmux_version() {
+    _stversion__supported="$(_get_digits_from_string_helper "${SUPPORTED_TMUX_VERSION}")"
+    if [ -z "${TMUX_VERSION}" ] || [ -z "$(_get_tmux_environment_helper "TMUX_VERSION")" ]; then
+        TMUX_VERSION="$(_get_digits_from_string_helper "$(tmux -V)")"
+        export TMUX_VERSION #speed up consecutive calls
+        tmux set-environment -g TMUX_VERSION "${TMUX_VERSION}"
+    fi
+
+    [ "${TMUX_VERSION}" -lt "${_stversion__supported}" ] && return 1 || return 0
 }
 
 # Ensures TMUX_PLUGIN_MANAGER_PATH global env variable is set.
